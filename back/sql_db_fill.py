755752b34e966946d3ffad0d5ca23ab98e3d5ff1
Dmitriy-Muchanovskiy <78701052+Dmitriy-Muchanovskiy@users.noreply.github.com>
2021-04-14 01:26:50 +0400

    Update sql_db_fill.py

--- back/sql_db_fill.py
+++ back/sql_db_fill.py
@@ -8,14 +8,13 @@ metadata = MetaData()
 projarka = Table('first', metadata,
   Column('id', Integer, primary_key=True),
   Column('first_text', String),
-  Column('sec_text', String),
 )
 
 engine = create_engine('mysql://root:rootdb@projarka-db/projarka')
 metadata.create_all(engine)
 
 with engine.connect() as con:
-  select = con.execute("insert into first(id,first_text,sec_text) values('1','This is the projarka-test!', 'This is the projarka sec-text!')")
+  select = con.execute("insert into first(id,first_text,sec_text) values('1','This is the projarka-test!')")
 
 
 
